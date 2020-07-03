2e167478a171983d1e5436f26081da2e88bd5878
Dikson Rajbanshi <34827312+Noskid1999@users.noreply.github.com>
2020-07-03 18:05:46 +0545

    Fix in timestamp

--- transcribe-app.js
+++ transcribe-app.js
@@ -888,7 +888,6 @@ app.post("/transcription-hr-review-table-datas", (req, res) => {
     from users_audio, users , audio WHERE
     users_audio.user_id = users.user_id
     AND users_audio.audio_id = audio.audio_id
-   
     AND users_audio.type="transcription"    
     ORDER BY users_audio.start_time DESC`;
 
