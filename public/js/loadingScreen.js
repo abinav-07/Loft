3e65c55209e2b054bbc13cfc7d3cfb3861c53558
abinav-07 <abighimire07@gmail.com>
2020-10-07 10:03:31 +0545

    OverLay Loading Screen in Lt

--- public/js/loadingScreen.js
+++ public/js/loadingScreen.js
@@ -2,7 +2,7 @@
 
 function UpdateLoadingFlag(Percentage) {
     if (document.getElementById("loading_flag")) {
-        document.getElementById("loading_flag").innerText = "Loading " + Percentage + "%";
+        document.getElementById("loading_flag").innerText = "Loading " + Percentage + "%...";
         if (Percentage >= 100) {
             document.getElementById("loading_flag").style.display = "none";
         } else {
